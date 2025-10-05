e99a94de518b6b2deeea967d824ad0df0f2d2388
asoderlind <axesode@gmail.com>
2025-10-05 14:44:18 +0200

    add flake config

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "My NixOS connfiguration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+        ];
+      };
+    };
+  }
