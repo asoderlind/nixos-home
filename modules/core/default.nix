589d1fe281a43e6f368cb0066eb410497ca45895
asoderlind <axesode@gmail.com>
2025-10-10 17:20:05 +0200

    add sddm and other things

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -9,11 +9,14 @@
     ./greetd.nix
     ./network.nix
     ./packages.nix
+    ./sddm.nix
     ./services.nix
     ./system.nix
     ./stylix.nix
     ./thunar.nix
     ./user.nix
+    # ./quickshell.nix -- not implemented yet
+    ./virtualisation.nix
     ./xserver.nix
      inputs.stylix.nixosModules.stylix
   ];
---
+++ modules/core/nh.nix
@@ -0,0 +1,20 @@
+# NH is a modern helper utility that implements tools from the NixOS ecosystem
+{ pkgs
+, username
+, ...
+}: {
+  programs.nh = {
+    enable = true;
+    clean = {
+      enable = true;
+      extraArgs = "--keep-since 7d --keep 5";
+    };
+    flake = "/home/${username}/zaneyos";
+  };
+
+  environment.systemPackages = with pkgs; [
+    nix-output-monitor
+    nvd
+  ];
+}
+
