4de9a2548986d0421cab004ce77e5440189bc39b
asoderlind <axesode@gmail.com>
2026-07-13 11:30:11 +0200

    fix: remove nextcloud from exec-once since it's called from another site

--- modules/home/hyprland/exec-once.nix
+++ modules/home/hyprland/exec-once.nix
@@ -10,7 +10,6 @@ in
       "wl-paste --type text --watch cliphist store" # Saves text
       "wl-paste --type image --watch cliphist store" # Saves images
       "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-      "nextcloud --background"
       "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
       "systemctl --user start hyprpolkitagent"
       "pypr &"
